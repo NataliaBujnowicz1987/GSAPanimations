b8654dd1f4f08d7d88b3835eb3e3b8fd4eab6ada
Natalia Bujnowicz <nataliabujnowicz1987@gmail.com>
2020-08-26 19:26:53 +0100

    Updates

---
+++ precache-manifest.ccdc025eca8224a6c15cdab3ec1b4563.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bcfc0acc01d42116d989f9a2cecd3b",
+    "url": "/GSAPanimations/index.html"
+  },
+  {
+    "revision": "eee6adb5d0029ad9bcd5",
+    "url": "/GSAPanimations/static/css/main.9d6d5858.chunk.css"
+  },
+  {
+    "revision": "a9527a4c120b931be825",
+    "url": "/GSAPanimations/static/js/2.c7fbaed7.chunk.js"
+  },
+  {
+    "revision": "0fb1ab9166e4090d40b886200af7baec",
+    "url": "/GSAPanimations/static/js/2.c7fbaed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee6adb5d0029ad9bcd5",
+    "url": "/GSAPanimations/static/js/main.f8ba100f.chunk.js"
+  },
+  {
+    "revision": "ebcdb99efc4294e145b7",
+    "url": "/GSAPanimations/static/js/runtime-main.e48c2d99.js"
+  },
+  {
+    "revision": "33670f7eb12b22d59d2381ac621a8952",
+    "url": "/GSAPanimations/static/media/arrowBack.33670f7e.png"
+  },
+  {
+    "revision": "31e3c68a5f42cdd5efa47e475f4c2200",
+    "url": "/GSAPanimations/static/media/arrowNext.31e3c68a.png"
+  },
+  {
+    "revision": "d56b6cb23960c16b24739e6b44b5f89c",
+    "url": "/GSAPanimations/static/media/ed.d56b6cb2.jpg"
+  },
+  {
+    "revision": "405ada860e09e4a82dbd22947e89d84b",
+    "url": "/GSAPanimations/static/media/fox.405ada86.svg"
+  },
+  {
+    "revision": "0bb2f24c6bf960fce9cc2c1e5e2f8214",
+    "url": "/GSAPanimations/static/media/gsap.0bb2f24c.png"
+  },
+  {
+    "revision": "355e6b955da7e5715358e28360d4e693",
+    "url": "/GSAPanimations/static/media/kevin.355e6b95.jpg"
+  },
+  {
+    "revision": "aad1d341a04d21c48f22383f21b1bcde",
+    "url": "/GSAPanimations/static/media/mosh.aad1d341.jpg"
+  },
+  {
+    "revision": "33a124dbb2d48c1082f982e21628ed36",
+    "url": "/GSAPanimations/static/media/paw.33a124db.svg"
+  },
+  {
+    "revision": "6628e39fb2ab3460d6c5d92cab8189ca",
+    "url": "/GSAPanimations/static/media/paw2.6628e39f.svg"
+  }
+]);
